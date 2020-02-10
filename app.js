22a63d0c9dd5fb1d253ca491b191879ee8784a58
will-ing <willkoger@gmail.com>
2020-02-10 13:01:43 -0800

    new branch

---
+++ app.js
@@ -0,0 +1,9 @@
+'strict'
+
+// needs to calculate the number of cookies each location must make every day
+// number of cookies to make depends on the hours of operation (6:00 AM to 8:00 PM for all locations)
+// The minimum number of customers per hour.
+// The maximum number of customers per hour.
+// The average number of cookies purchased per customer.
+// Pat will need to be able to add and remove locations from the daily projections report,
+// modify the input numbers for each location based on day of the week, special events, and other factors. 
