b0560908c8d0edc32aa4748a2aa0bad2a257ee8f
will-ing <willkoger@gmail.com>
2020-02-12 06:39:09 -0800

    restrutured for readability

--- app.js
+++ app.js
@@ -6,10 +6,11 @@
 
 // Pat will need to be able to add and remove locations from the daily projections report,
 
-
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
 
 var dayOfWeek = ['sun', 'mon', 'tues', 'wed', 'thur', 'fri', 'sat']
+
+var storeArr = []; // used to store objects in an arr. func ttlByHour needs this to run its loop.
 
 function numberOfCookiesNeeded(arr){ //sums up an array
   var i = 0;
@@ -20,20 +21,6 @@ function numberOfCookiesNeeded(arr){ //sums up an array
   }
   return(ttl);
 }
-
-// generates cookies purchased by hour
-function cookiesPurchased(enterStore){ // enter the store that the const built
-  var cookiesUPH = 0 // starts the loop
-  while (cookiesUPH < hours.length) { // when cookiesUPH is less than hours arr run loop
-    var cookiesPerHour = Math.floor((Math.random() * (enterStore.maxCustomers - enterStore.minCustomers) + enterStore.minCustomers)) * Math.floor(enterStore.avgCookies); // creates random cookies per hour var that multiplies random number and avgCookies
-    hours[cookiesUPH] += ' ' + cookiesPerHour; // creates concatenation for hours arr & cookiesPerHour var
-    enterStore.cookiesSold.push(cookiesPerHour); // pushes random number to arr in each store object
-    cookiesUPH++ //increments var to loop through hour arr
-  }
-  return(enterStore.cookiesPerHour);
-}
-
-var storeArr = []; // used to store objects in an arr. func ttlByHour needs this to run its loop.
 
 var createStore = function(name, maxCustomers, minCustomers, avgCookies, cookiesSold){ // constr func to build objects based on imputed properties
   this.name = name;
@@ -68,25 +55,34 @@ function createHead(){ //creates head of table
       rowOne.appendChild(head); 
 }
 
+// generates cookies purchased by hour
+function cookiesPurchased(enterStore){ // enter the store that the const built
+  var cookiesUPH = 0 // starts the loop
+  while (cookiesUPH < hours.length) { // when cookiesUPH is less than hours arr run loop
+    var cookiesPerHour = Math.floor((Math.random() * (enterStore.maxCustomers - enterStore.minCustomers) + enterStore.minCustomers)) * Math.floor(enterStore.avgCookies); // creates random cookies per hour var that multiplies random number and avgCookies
+    hours[cookiesUPH] += ' ' + cookiesPerHour; // creates concatenation for hours arr & cookiesPerHour var
+    enterStore.cookiesSold.push(cookiesPerHour); // pushes random number to arr in each store object
+    cookiesUPH++ //increments var to loop through hour arr
+  }
+  return(enterStore.cookiesPerHour);
+}
 
-function rowRow(allTheCookies){// creates rows based on store entered
-  var table = document.getElementById('tableOne')
-  var rowOne = document.createElement('tr');
-  table.appendChild(rowOne); 
-  var rows = document.createElement('td');
-  rows.textContent = allTheCookies.name; 
-  rowOne.appendChild(rows); 
-  for (k = 0; k < allTheCookies.cookiesSold.length; k++){
+// pushes total of cookies sold per store to the end of the arr.
+function totalByStore(stor){
+  stor.cookiesSold.push(numberOfCookiesNeeded(stor.cookiesSold));
+}
+function rowRow(allTheCookies){// creates rows based on stores entered
+  var table = document.getElementById('tableOne') // selecting tableOne
+  var rowOne = document.createElement('tr'); // create table row as rowOne
+  table.appendChild(rowOne); // row is appended
+  var rows = document.createElement('td'); // creates an element for table data
+  rows.textContent = allTheCookies.name; // assigns first row "hour ttls"
+  rowOne.appendChild(rows); // add 'rows' content to first <td> in row
+  for (k = 0; k < allTheCookies.cookiesSold.length; k++){ 
     var rows = document.createElement('td');
     rows.textContent = allTheCookies.cookiesSold[k]; 
     rowOne.appendChild(rows); 
   }
-
-}
-// pushes total of cookies sold to the end of the arr.
-function totalByStore(stor){
-  stor.cookiesSold.push(numberOfCookiesNeeded(stor.cookiesSold));
-  
 }
 
 function ttlByHour(){
@@ -114,7 +110,7 @@ function overAllTtl(){ // needs to get total for all hours and stores; prototype
   numberOfCookiesNeeded(paris.cookiesSold) +
   numberOfCookiesNeeded(lima.cookiesSold); // works; should refactor to loop
 
-  // Need to reference last part of the page
+  // Need to print on last cell of the page
  
 }
 
@@ -126,7 +122,6 @@ function dry(arr) {
     rowRow(arr[t]) // displays the store argument entered and displays the name of the store followed by the cookies sold by hour
   }
 }
-
 
 createHead(); // func makes header of table
 
