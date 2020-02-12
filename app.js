eadbee66856f4ee5a391a57071aa99889f69aabe
will-ing <willkoger@gmail.com>
2020-02-12 06:18:42 -0800

    did some dry work

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ var createStore = function(name, maxCustomers, minCustomers, avgCookies, cookies
 
 }
 // creates seattle object
-var seattle = new createStore('seattle', 65, 23, 6.3, []);// creates store object and its properties
+var seattle = new createStore('seattle', 65, 23, 6.3, []);// creates store object and its properties using constructor
 var tokyo = new createStore('tokyo', 24, 3, 1.2,[]);
 var dubai = new createStore('dubai', 28, 11, 3.7, []);
 var paris = new createStore('paris', 38, 20, 2.3, []);
@@ -99,7 +99,7 @@ function ttlByHour(){
   for ( w = 0; w < hours.length; w++) {// loops through hours arr for ttl hrs
     var ttl = 0 // ttl holds sum by hour and then assigns it to hour the loop is on
     for (q = 0; q < storeArr.length; q++){ // loops through storeArr for stores that are created and creates <td> for the row.
-      ttl = ttl + storeArr[q].cookiesSold[w]; // adds each stores ttl per hour on each hour
+      ttl = ttl + storeArr[q].cookiesSold[w]; // adds stores combined ttl per hour on each hour
     }
     var rows = document.createElement('td'); // creates an element for table column
     rows.textContent = ttl; // assigns it ttl
@@ -107,39 +107,32 @@ function ttlByHour(){
   }
 }
 
-function overAllTtl(){
+function overAllTtl(){ // needs to get total for all hours and stores; prototype maybe? --brocken--
   var ttlCookies = numberOfCookiesNeeded(seattle.cookiesSold) +
   numberOfCookiesNeeded(tokyo.cookiesSold) +
   numberOfCookiesNeeded(dubai.cookiesSold) +
   numberOfCookiesNeeded(paris.cookiesSold) +
-  numberOfCookiesNeeded(lima.cookiesSold);
+  numberOfCookiesNeeded(lima.cookiesSold); // works; should refactor to loop
+
+  // Need to reference last part of the page
  
 }
 
+function dry(arr) {
+  console.log(arr);
+  for (t = 0; t < arr.length; t++){
+    cookiesPurchased(arr[t]); // generates random number of cookies by hour and stores in each object arr
+    totalByStore(arr[t]); // Sums up arr of random cookie sales and adds it ot the end of the arr
+    rowRow(arr[t]) // displays the store argument entered and displays the name of the store followed by the cookies sold by hour
+  }
+}
 
-createHead();
 
-cookiesPurchased(seattle); // generates random number of cookies by hour and stores in each object arr
-totalByStore(seattle); // Sums up arr of random cookie sales and adds it ot the end of the arr
-rowRow(seattle); // displays the store argument entered and displays the name of the store followed by the cookies sold by hour
+createHead(); // func makes header of table
 
-cookiesPurchased(tokyo);
-totalByStore(tokyo);
-rowRow(tokyo);
+dry(storeArr); // runs 3 func in to loop for rows
 
-cookiesPurchased(dubai);
-totalByStore(dubai);
-rowRow(dubai);
-
-cookiesPurchased(paris);
-totalByStore(paris);
-rowRow(paris);
-
-cookiesPurchased(lima);
-totalByStore(lima);
-rowRow(lima);
-
-ttlByHour();
+ttlByHour(); // Last row  on table with hourly ttl
 
 // overAllTtl();
 
