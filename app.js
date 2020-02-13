548f3a59f4999447b748f582815ccb9cbfc6647f
will-ing <willkoger@gmail.com>
2020-02-12 18:48:58 -0800

    did some dry on js code

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var dayOfWeek = ['sun', 'mon', 'tues', 'wed', 'thur', 'fri', 'sat']
 
 var storeArr = []; // used to store objects in an arr. func ttlByHour needs this to run its loop.
 
+var table = document.getElementById('tableOne') // finds id to reference
+
 function numberOfCookiesNeeded(arr){ //sums up an array
   var i = 0;
   var ttl = 0;
@@ -22,24 +24,23 @@ function numberOfCookiesNeeded(arr){ //sums up an array
   return(ttl);
 }
 
-var createStore = function(name, maxCustomers, minCustomers, avgCookies, cookiesSold){ // constr func to build objects based on imputed properties
+var createStore = function(name, maxCustomers, minCustomers, avgCookies){ // constr func to build objects based on imputed properties
   this.name = name;
   this.maxCustomers = maxCustomers;
   this.minCustomers = minCustomers;
   this.avgCookies = avgCookies;
-  this.cookiesSold = cookiesSold;
+  this.cookiesSold = [];
   storeArr.push(this); // pushes all properties to arr
 
 }
 // creates seattle object
-var seattle = new createStore('seattle', 65, 23, 6.3, []);// creates store object and its properties using constructor
-var tokyo = new createStore('tokyo', 24, 3, 1.2,[]);
-var dubai = new createStore('dubai', 28, 11, 3.7, []);
-var paris = new createStore('paris', 38, 20, 2.3, []);
-var lima = new createStore('lima', 16, 2, 4.6, []);
+var seattle = new createStore('seattle', 65, 23, 6.3);// creates store object and its properties using constructor
+var tokyo = new createStore('tokyo', 24, 3, 1.2);
+var dubai = new createStore('dubai', 28, 11, 3.7);
+var paris = new createStore('paris', 38, 20, 2.3);
+var lima = new createStore('lima', 16, 2, 4.6);
 
 function createHead(){ //creates head of table
-  var table = document.getElementById('tableOne') // finds id to reference
   var rowOne = document.createElement('tr'); 
   table.appendChild(rowOne); // this makes the rows
   var head = document.createElement('th');
@@ -51,8 +52,8 @@ function createHead(){ //creates head of table
       rowOne.appendChild(head); // adds it to the head
     }
     var head = document.createElement('th');
-      head.textContent = 'Total Cookies'; // identify what goes in the head
-      rowOne.appendChild(head); 
+    head.textContent = 'Total Cookies'; // identify what goes in the head
+    rowOne.appendChild(head); 
 }
 
 // generates cookies purchased by hour
@@ -60,7 +61,7 @@ function cookiesPurchased(enterStore){ // enter the store that the const built
   var cookiesUPH = 0 // starts the loop
   while (cookiesUPH < hours.length) { // when cookiesUPH is less than hours arr run loop
     var cookiesPerHour = Math.floor((Math.random() * (enterStore.maxCustomers - enterStore.minCustomers) + enterStore.minCustomers)) * Math.floor(enterStore.avgCookies); // creates random cookies per hour var that multiplies random number and avgCookies
-    hours[cookiesUPH] += ' ' + cookiesPerHour; // creates concatenation for hours arr & cookiesPerHour var
+    // hours[cookiesUPH] += ' ' + cookiesPerHour; // creates concatenation for hours arr & cookiesPerHour var // <--Delete -->
     enterStore.cookiesSold.push(cookiesPerHour); // pushes random number to arr in each store object
     cookiesUPH++ //increments var to loop through hour arr
   }
@@ -72,7 +73,6 @@ function totalByStore(stor){
   stor.cookiesSold.push(numberOfCookiesNeeded(stor.cookiesSold));
 }
 function rowRow(allTheCookies){// creates rows based on stores entered
-  var table = document.getElementById('tableOne') // selecting tableOne
   var rowOne = document.createElement('tr'); // create table row as rowOne
   table.appendChild(rowOne); // row is appended
   var rows = document.createElement('td'); // creates an element for table data
@@ -86,7 +86,6 @@ function rowRow(allTheCookies){// creates rows based on stores entered
 }
 
 function ttlByHour(){
-  var table = document.getElementById('tableOne') // select table one
   var rowOne = document.createElement('tr'); // create table row as rowOne
   table.appendChild(rowOne); // row is appended
   var rows = document.createElement('td'); // creates an element for table data
@@ -101,6 +100,9 @@ function ttlByHour(){
     rows.textContent = ttl; // assigns it ttl
     rowOne.appendChild(rows); // adds values of ttl to HTML table
   }
+  var rows = document.createElement('td');
+  rows.textContent = overAllTtl();
+  rowOne.appendChild(rows);
 }
 
 function overAllTtl(){ // needs to get total for all hours and stores; prototype maybe? --brocken--
@@ -109,13 +111,12 @@ function overAllTtl(){ // needs to get total for all hours and stores; prototype
   numberOfCookiesNeeded(dubai.cookiesSold) +
   numberOfCookiesNeeded(paris.cookiesSold) +
   numberOfCookiesNeeded(lima.cookiesSold); // works; should refactor to loop
-
+  return(ttlCookies);
   // Need to print on last cell of the page
- 
+  
 }
 
 function dry(arr) {
-  console.log(arr);
   for (var t = 0; t < arr.length; t++){
     cookiesPurchased(arr[t]); // generates random number of cookies by hour and stores in each object arr
     totalByStore(arr[t]); // Sums up arr of random cookie sales and adds it ot the end of the arr
@@ -134,3 +135,16 @@ ttlByHour(); // Last row  on table with hourly ttl
 
 // calculate how many Salmon Cookie Tossers are needed at each location each hour.
 // Salmon Cookie Tosser can serve 20 customers per hour
+createStore.prototype.cookiesTosser = function(){
+  var i = 0;
+  while(i < hours.length){
+    var result = this.cookiesSold.slice(0, 15)) / 20;
+    i++
+  }
+  console.log(this.cookiesSold.slice(0, 15));
+  console.log(result);
+  
+  return(result);
+   
+}
+seattle.cookiesTosser();
